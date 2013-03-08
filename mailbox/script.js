b656a9eb48838e2315e5ea8ba2cbc258c23484c4
user26 <user26@user26-HPElite-8300-MT.(none)>
2013-03-08 11:46:40 +0530

    bootstrap added

--- mailbox/script.js
+++ mailbox/script.js
@@ -1,1 +1,11 @@
 var mails=[{'from':'asdadsa','from_email':'asdasdasdasd','subject':'asdcasdcasdcasd','body':'asdfasdfasdf'}];
+var tags=['inbox','draft','sent'];
+window.onload=init;
+function init(){
+}
+function populate(){
+	var mailbox=document.getElementById("mails");
+	for(var i=0; i<mails.length;i++){
+		document.createElement("span");
+	}
+}
