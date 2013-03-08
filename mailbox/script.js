171dd1d3626e9e4d6dffad4c8f0d9e94fac9a806
user26 <user26@user26-HPElite-8300-MT.(none)>
2013-03-08 11:51:58 +0530

    some changes, append to next sibling

--- mailbox/script.js
+++ mailbox/script.js
@@ -2,9 +2,11 @@ var mails=[{'from':'asdadsa','from_email':'asdasdasdasd','subject':'asdcasdcasdc
 var tags=['inbox','draft','sent'];
 window.onload=init;
 function init(){
+	populate();
 }
 function populate(){
 	var mailbox=document.getElementById("mails");
+              <li><a href="#">Link</a></li>
 	for(var i=0; i<mails.length;i++){
 		document.createElement("span");
 	}
